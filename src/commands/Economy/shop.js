9c6d642e7ecaaa0b7b7a1bba8fefa5ea052be463
aceandriel-pixel <aceandriel@gmail.com>
2026-08-05 20:45:24 +0800

    Update shop.js

--- src/commands/Economy/shop.js
+++ src/commands/Economy/shop.js
@@ -1,13 +1,40 @@
-import { SlashCommandBuilder } from 'discord.js';
-import shopBrowse from './modules/shop_browse.js';
+import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } from 'discord.js';
+import { botConfig } from '../../config/bot.js';
 
 export default {
-    slashOnly: true,
     data: new SlashCommandBuilder()
         .setName('shop')
-        .setDescription('Browse the economy shop.'),
+        .setDescription('Browse the arcane vaults and shops')
+        .addStringOption(option =>
+            option.setName('category')
+                .setDescription('Select which shop to browse')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Arcane Vault (Mana Storage)', value: 'shop1' },
+                    { name: 'Custom Requests', value: 'shop2' }
+                )),
 
-    async execute(interaction, config, client) {
-        return shopBrowse.execute(interaction, config, client);
-    },
+    async execute(interaction) {
+        const selectedShopKey = interaction.options.getString('category');
+        const shopData = botConfig.shop[selectedShopKey];
+
+        if (!shopData) {
+            return interaction.reply({ content: 'Shop not found!', ephemeral: true });
+        }
+
+        const embed = new EmbedBuilder()
+            .setTitle(shopData.title)
+            .setDescription(`Currency used: **${shopData.currency}**`)
+            .setColor(botConfig.embeds.colors.economy);
+
+        shopData.items.forEach((item, index) => {
+            embed.addFields({
+                name: `${index + 1}. ${item.name} - ${item.price.toLocaleString()} ${shopData.currency}`,
+                value: `${item.description} ${item.capacityBoost ? `\n*Capacity Boost: +${item.capacityBoost.toLocaleString()} Mana*` : ''}`,
+                inline: false
+            });
+        });
+
+        await interaction.reply({ embeds: [embed] });
+    }
 };
